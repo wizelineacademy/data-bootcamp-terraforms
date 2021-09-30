364514888d308203b68b9fc9a1c534dc85da03fb
ingchh <joaquin.chiacchio@wizeline.com>
2021-09-30 11:06:21 -0300

    Networking output.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,9 +1,8 @@
 module "networking" {
-
   source                 = "./modules/networking"
-  destination_cidr_block = var.destination_cidr_block
   vpc_cidr               = var.vpc_cidr
   public_subnets_cidr    = var.public_subnets_cidr
   private_subnets_cidr   = var.private_subnets_cidr
   availability_zones     = var.availability_zones
+  destination_cidr_block = var.destination_cidr_block
 }
