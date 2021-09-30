4cf68b08c7d1263b89456cdb2f266888d74bd307
Eduardo mendoza <garibo.eduardo90@gmail.com>
2021-09-30 11:35:04 -0700

    Added rds module

---
+++ aws/modules/rds/main.tf
@@ -0,0 +1,30 @@
+# --- main.tf/modules/aws/rds
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "rds-subnet"
+  subnet_ids = [
+    var.private_subnet]
+
+  tags = {
+    Name = "rds-db"
+  }
+}
+resource "aws_db_instance" "rds-instance" {
+  allocated_storage      = var.allocated_storage
+  storage_type           = "gp2"
+  engine                 = var.db_engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_type
+  name                   = var.database_name
+  username               = var.db_username
+  password               = var.db_password
+  parameter_group_name   = "default.mysql5.7"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
+  publicly_accessible = var.publicly_accessible
+  vpc_security_group_ids = [
+    var.service_sg]
+
+  tags = {
+    Name = "rds-database"
+  }
+}
