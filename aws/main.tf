74a0664dc8b9069bab1aa06606e86cc429692d3d
ingchh <joaquin.chiacchio@wizeline.com>
2021-10-01 15:24:58 -0300

    root changes

--- aws/main.tf
+++ aws/main.tf
@@ -1,60 +1,65 @@
 
 module "networking" {
-  source                 = "./modules/networking"
-  vpc_cidr               = var.vpc_cidr
-  public_subnets_cidr    = var.public_subnets_cidr
-  private_subnets_cidr   = var.private_subnets_cidr
-  availability_zones     = var.availability_zones
-  destination_cidr_block = var.destination_cidr_block
+  source = "./modules/networking"
+
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
 }
 
 
 module "eks" {
   source = "./modules/eks"
-  
-  region          = var.region
+
+  region = var.region
+  vpc_id = module.networking.vpc_id
+  subnet = module.networking.private_subnets_ids
+
   cluster_name    = var.cluster_name
-  cluster_version = "1.20"
-  vpc_id          = module.networking.vpc_id
-  subnets         = module.networking.private_subnets_ids
+  cluster_version = var.cluster_version
 
-  instance_type_group1        = "t2.small"
-  instance_type_group2        = "t2.medium"
-  asg_desired_capacity_group1 = 2
-  asg_desired_capacity_group2 = 1
-
-  
+  instance_type_group1        = var.instance_type_group1
+  instance_type_group2        = var.instance_type_group2
+  asg_desired_capacity_group1 = var.asg_desired_capacity_group1
+  asg_desired_capacity_group2 = var.asg_desired_capacity_group2
 }
 module "ec2" {
   source = "./modules/ec2"
+
+  region    = var.region
+  vpc_id    = module.networking.vpc_id
   subnet_id = module.networking.private_subnets_ids
-  vpc_id = module.networking.vpc_id
-  instance_count = var.instance_count
-  ec2_name = var.ec2_name 
-  ec2_security_group_name = var.ec2_security_group_name
+
+  ec2_name                       = var.ec2_name
+  ec2_security_group_name        = var.ec2_security_group_name
   ec2_security_group_description = var.ec2_security_group_description
-  ec2_ami = var.ec2_ami
-  ec2_instance_type = var.ec2_instance_type
-  ec2_ssh_key_name = var.ec2_ssh_public_key_path
+  ec2_ami                        = var.ec2_ami
+  ec2_instance_type              = var.ec2_instance_type
+  ec2_ssh_key_name               = var.ec2_ssh_public_key_path
 }
 
 module "rds" {
   source = "./modules/rds"
-  private_subnet = module.networking.private_subnets_cidr
-  allocated_storage = var.allocated_storage
-  db_engine = var.db_engine
-  engine_version = var.engine_version
-  instance_type = var.instance_type
-  database_name = var.database_name
-  db_username = var.db_username
-  db_password = var.db_password
-  service_sg = var.service_sg
+
+  region         = var.region
+  vpc_id         = module.networking.vpc_id
+  private_subnet = module.networking.private_subnets_ids
+
+  allocated_storage   = var.allocated_storage
+  db_engine           = var.db_engine
+  engine_version      = var.engine_version
+  instance_type       = var.instance_type
+  database_name       = var.database_name
+  db_username         = var.db_username
+  db_password         = var.db_password
+  service_sg          = var.service_sg
   publicly_accessible = var.publicly_accessible
 }
 
 module "s3" {
   source = "./modules/s3"
+
   buckebucket_prefix = var.buckebucket_prefix
-  acl = var.acl
-  versioning = var.versioning
+  acl                = var.acl
+  versioning         = var.versioning
 }
--- aws/variables.tf
+++ aws/variables.tf
@@ -106,8 +106,6 @@ variable "cluster_name" {
   type        = string
 }
 
-### RDS ###
-
 ### S3 ###
 variable "bucket_prefix" {
   type = string
@@ -119,4 +117,49 @@ variable "acl" {
 
 variable "versioning" {
   type = bool
+}
+
+### eks ###
+variable "region" {
+  default     = "us-west-2"
+  description = "AWS region"
+}
+variable "cluster_name" {
+  description = "The name of the Airflow cluster (e.g. airflow-xyz). This variable is used to namespace all resources created by this module."
+  type        = string
+}
+
+variable "cluster_version" {
+  default     = "1.20"
+  description = "Cluster version"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC where the cluster and workers will be deployed."
+}
+
+variable "subnet" {
+  type        = string
+  description = "The private subnet to place the EKS cluster and workers within."
+}
+
+variable "instance_type_group1" {
+  default     = "t2.small"
+  description = "Instance type for the group 1"
+}
+
+variable "instance_type_group2" {
+  default     = "t2.medium"
+  description = "Instance type for the group 2"
+}
+
+variable "asg_desired_capacity_group1" {
+  default     = 2
+  description = "Desired capacity for autoscaling for the group 1"
+}
+
+variable "asg_desired_capacity_group2" {
+  default     = 1
+  description = "Desired capacity for autoscaling for the group 2"
 }
